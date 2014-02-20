28f0d47dbd53c4d62a466bd371449283411fc194
Derek <derekweatherford@gmail.com>
2014-02-19 19:22:15 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CyclingDashboard::Application.config.secret_key_base = '0e4512a0b954439b47943366fc61534c62ccf13801629a27e73fb7a6567d8d2e7b41ca2cecd7723c6956674b11232d4df05275cacf7713a9371a49af3787f84a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CyclingDashboard::Application.config.session_store :cookie_store, key: '_cyclingDashboard_session'
